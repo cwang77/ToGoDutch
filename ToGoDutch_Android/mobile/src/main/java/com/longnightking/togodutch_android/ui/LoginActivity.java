1fd8dd727daa23af84636161a9f1e40977152197
Changye Wang <davidwang623@gmail.com>
2015-05-06 00:37:03 -0700

    add sign up ui

--- ToGoDutch_Android/mobile/src/main/java/com/longnightking/togodutch_android/ui/LoginActivity.java
+++ ToGoDutch_Android/mobile/src/main/java/com/longnightking/togodutch_android/ui/LoginActivity.java
@@ -2,9 +2,14 @@ package com.longnightking.togodutch_android.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.facebook.login.widget.LoginButton;
 import com.longnightking.togodutch_android.R;
@@ -19,7 +24,16 @@ import com.parse.ParseUser;
  * <p/>
  */
 public class LoginActivity extends Activity {
+
     private LoginButton faceBookLoginBtn;
+
+    private TextView forgotPswdTxt, signUpTxt;
+
+    private Button signInBtn;
+
+    private AutoCompleteTextView emailTxt;
+
+    private EditText passwordTxt;
 
     public static final String TAG = LoginActivity.class.getName();
 
@@ -52,28 +66,58 @@ public class LoginActivity extends Activity {
     }
 
     private void bindViews(){
+        forgotPswdTxt = (TextView)findViewById(R.id.forgotPasswordTxt);
+        signUpTxt = (TextView)findViewById(R.id.signUpTxt);
+        emailTxt = (AutoCompleteTextView)findViewById(R.id.email);
+        passwordTxt = (EditText)findViewById(R.id.password);
+        signInBtn = (Button)findViewById(R.id.email_sign_in_button);
         faceBookLoginBtn = (LoginButton)findViewById(R.id.fb_login_button);
 
-        faceBookLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this, UserManagement.getInstance(LoginActivity.this).getPermissionsList(), new LogInCallback() {
-                    @Override
-                    public void done(ParseUser user, ParseException err) {
-                        if (user == null) {
-                            Log.d(TAG, "Uh oh. The user cancelled the Facebook login.");
-                        } else {
-                            if (user.isNew()) {
-                                Log.d(TAG, "User signed up and logged in through Facebook!");
+        appendUnderlineForTxtView(forgotPswdTxt);
+        appendUnderlineForTxtView(signUpTxt);
+
+        forgotPswdTxt.setOnClickListener(mViewClickListener);
+        signUpTxt.setOnClickListener(mViewClickListener);
+        signInBtn.setOnClickListener(mViewClickListener);
+        faceBookLoginBtn.setOnClickListener(mViewClickListener);
+    }
+
+    private View.OnClickListener mViewClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.forgotPasswordTxt:
+                    break;
+                case R.id.signUpTxt:
+                    Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.email_sign_in_button:
+                    break;
+                case R.id.fb_login_button:
+                    ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this, UserManagement.getInstance(LoginActivity.this).getPermissionsList(), new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException err) {
+                            if (user == null) {
+                                Log.d(TAG, "Uh oh. The user cancelled the Facebook login.");
                             } else {
-                                Log.d(TAG, "User logged in through Facebook!");
+                                if (user.isNew()) {
+                                    Log.d(TAG, "User signed up and logged in through Facebook!");
+                                } else {
+                                    Log.d(TAG, "User logged in through Facebook!");
+                                }
+                                reDirectToMainActivity();
                             }
-                            reDirectToMainActivity();
                         }
-                    }
-                });
+                    });
+                    break;
+                default: break;
             }
-        });
+        }
+    };
+
+    private void appendUnderlineForTxtView(TextView txtView){
+        txtView.setPaintFlags(txtView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 
     private void reDirectToMainActivity(){
---
+++ ToGoDutch_Android/mobile/src/main/java/com/longnightking/togodutch_android/ui/SignUpActivity.java
@@ -0,0 +1,84 @@
+package com.longnightking.togodutch_android.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.longnightking.togodutch_android.R;
+
+public class SignUpActivity extends ActionBarActivity {
+
+    private EditText username, password, passwordRepeat;
+
+    private AutoCompleteTextView email;
+
+    private Button registerBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        bindViews();
+    }
+
+    private void bindViews(){
+        email = (AutoCompleteTextView)findViewById(R.id.email);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        passwordRepeat = (EditText)findViewById(R.id.password_repeat);
+        registerBtn = (Button)findViewById(R.id.register_button);
+        registerBtn.setEnabled(false);
+
+        email.setOnFocusChangeListener(mFocusChangeListener);
+        username.setOnFocusChangeListener(mFocusChangeListener);
+        password.setOnFocusChangeListener(mFocusChangeListener);
+        passwordRepeat.setOnFocusChangeListener(mFocusChangeListener);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerBtn.setEnabled(false);
+            }
+        });
+    }
+
+    private View.OnFocusChangeListener mFocusChangeListener = new View.OnFocusChangeListener() {
+
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (!hasFocus) {
+                EditText thisEditTxt = (EditText)v;
+                if(thisEditTxt.getText().length() <= 0)
+;
+            }else{
+
+            }
+
+        }
+    };
+
+    private TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+}
